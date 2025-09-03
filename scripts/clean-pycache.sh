1828aceac0d0af31cc9f69a3d14c55ddce643582
siriknikita <mykyta.siryk@lnu.edu.ua>
2025-09-03 17:55:38 +0300

    feat: Implement histogram computation & project reorganization
    - This commit intoduces a new way of handling histograms computations
    - Integrates seamlessly with previously developed code
    - Introduces major improvements in project organization
    - Adds a separation of concerns & concepts

---
+++ scripts/clean-pycache.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# This script removes all __pycache__ directories and .pyc files from the current directory and its subdirectories.
+# Usage: ./clean-pycache.sh
+
+find . -type d -name "__pycache__" -exec rm -r {} +
